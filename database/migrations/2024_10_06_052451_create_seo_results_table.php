0e55af830eb5dd00dc4a5c79cc6741adfb4da9a2
AJ Javadi <amirjavadi25@gmail.com>
2024-10-06 00:39:15 -0500

    seo results table migrations

--- database/migrations/2024_10_06_052451_create_seo_results_table.php
+++ database/migrations/2024_10_06_052451_create_seo_results_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('seo_results', function (Blueprint $table) {
             $table->id();
+            $table->string('url')->unique();
+            $table->float('aria_landmarks');
+            $table->float('img_alt_tags');
+            $table->float('color_contrast');
+            $table->float('semantic_html');
+            $table->float('broken_links');
+            $table->float('meta_tags');
+            $table->float('lazy_loading_images');
+            $table->float('favicon');
+            $table->float('mobile_friendly');
+            $table->float('input_types');
+            $table->float('total_score');
             $table->timestamps();
         });
     }
